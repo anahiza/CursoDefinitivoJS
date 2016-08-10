27fe979596447c7b80da02b0fd7ecfa2e1707402
Anhy <anahi.z.a@gmail.com>
2016-08-10 14:28:36 -0500

    Agregando titulos a las páginas con 'title'

--- src/signup/index.js
+++ src/signup/index.js
@@ -1,8 +1,11 @@
 var page = require('page');
 var template=require('./template');
 var empty= require('empty-element');
+var title=require('title');
 
 page('/signup', function(ctx,next){
+	title('Platzigram - Sign Up');
+
 	var main = document.getElementById('main-container');
   empty(main).appendChild(template);
 });
