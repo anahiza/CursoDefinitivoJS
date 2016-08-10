d4eab1329dec0cf27ee72e373aa21231a59577d9
Anhy <anahi.z.a@gmail.com>
2016-08-10 12:09:17 -0500

    Agregando Sign in

--- src/signin/template.js
+++ src/signin/template.js
@@ -1,7 +1,8 @@
 var yo= require('yo-yo');
 var landing = require('../landing');
+var empty= require('empty-element');
 
-var box= yo`<div class="col s12 m7">
+var signinForm= yo`<div class="col s12 m7">
               <div class="row">
                 <div class="signup-box">
                   <h1 class="platzigram">Platzigram</h1>
@@ -12,20 +13,19 @@ var box= yo`<div class="col s12 m7">
                       <a class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
                     </div>
                     <div class="divider"></div>
-                    <div class="section">
-                      <input type="email" name="email" placeholder="Correo electrónico"/>
+                    <div class="section">          
                       <input type="text" name="name" placeholder="Nombre completo"/>
                       <input type="text" name="username" placeholder="Nombre de usuario"/>
                       <input type="password" name="password" placeholder="Contraseña"/>
-                      <button class="btn waves-effect waves-light btn-signup" type="submit">Regístrate</button>
+                      <button class="btn waves-effect waves-light btn-signup" type="submit">Inicia Sesión</button>
                     </div>
                   </form>
                 </div>
               </div>
               <div class="row">
                 <div class="login-box">
-                  ¿Tienes una cuenta? <a href="/signin">Entrar</a>
+                  ¿No tienes una cuenta? <a href="/signup">Regístrate</a>
                 </div>
               </div>           
             </div>`;
-module.exports=landing(box);
+module.exports=landing(signinForm);
