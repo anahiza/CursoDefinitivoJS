4083d4b68d30d4c217402ff4baec8f0dd818b20e
Anhy <anahi.z.a@gmail.com>
2016-08-15 08:18:10 -0500

    Corregido picture-card

--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,51 +1,49 @@
-var yo=require('yo-yo') ;
-//Para compatibilidad con SAFARI
-// if (!window.Intl){
-//   window.Intl=require('intl');
+var yo = require('yo-yo');
+//Para compatibilidad con safari
+// if (!window.Intl) {
+//   window.Intl = require('intl');
 //   require('intl/locale-data/jsonp/en-US.js');
-//   require('intl/locale-data/jsonp/es-AR.js');
-  
+//   require('intl/locale-data/jsonp/es.js');
 // }
 
-var IntlRelativeFormat = window.IntlRelativeFormat=require('intl-relativeformat');
+var IntlRelativeFormat = window.IntlRelativeFormat = require('intl-relativeformat');
 
 require('intl-relativeformat/dist/locale-data/en.js');
 require('intl-relativeformat/dist/locale-data/es.js');
 
-var rf = new IntlRelativeFormat('es-AR');
+var rf = new IntlRelativeFormat('es');
 
-
-module.exports=function pictureCard(pic){
+module.exports = function pictureCard(pic) {
   var el;
 
-  function render(picture){
-    return yo`<div class="card ${picture.liked ? 'liked':''}">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="${picture.url}">
-        </div>
-        <div class="card-content">
-          <a href="user/${picture.user.username}" class="card-title">
-            <img src="${picture.user.avatar}" class="avatar" />
-            <span class="username">${picture.user.username}</span>  
-          </a>
-          <small class="right time">${rf.format(picture.createdAt)}</small>
-          <p>
-            <a class="left" href="#" onclick=${like.bind(null,true)}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-            <a class="left" href="#" onclick=${like.bind(null,false)}><i class="fa fa-heart" aria-hidden="true"></i></a>
-            <span class="left likes">${picture.likes} me gusta </span>
-          </p>
-        </div>
-      </div>`;
+  function render(picture) {
+    return yo`<div class="card ${picture.liked ? 'liked' : ''}">
+      <div class="card-image">
+        <img class="activator" src="${picture.url}">
+      </div>
+      <div class="card-content">
+        <a href="/user/${picture.user.username}" class="card-title">
+          <img src="${picture.user.avatar}" class="avatar" />
+          <span class="username">${picture.user.username}</span>
+        </a>
+        <small class="right time">${rf.format(picture.createdAt)}</small>
+        <p>
+          <a class="left" href="#" onclick=${like.bind(null, true)}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+          <a class="left" href="#" onclick=${like.bind(null, false)}><i class="fa fa-heart" aria-hidden="true"></i></a>
+          <span class="left likes">${picture.likes} me gusta</span>
+        </p>
+      </div>
+    </div>`
   }
 
-  function like(liked){
-    pic.liked=liked;
-    pic.likes+=liked ? 1 : -1;
-    var newEl=render(pic);
+  function like(liked) {
+    pic.liked = liked;
+    pic.likes += liked ? 1 : -1;
+    var newEl = render(pic);
     yo.update(el, newEl);
     return false;
   }
 
-  el=render(pic);
-  return el;  
+  el = render(pic);
+  return el;
 }
