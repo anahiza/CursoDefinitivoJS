06d9fb30bcf61c9286339cf8ff683c11b5a5df43
Anhy <anahi.z.a@gmail.com>
2016-08-16 18:05:32 -0500

    Obteniendo una respuesta del servidor con Superagent

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ app.set('view engine','pug');
 
 app.use(express.static('public'));
 
-app.get('/',function(req, res){
+
+app.get('/', function(req, res){
 	res.render('index',{title: 'Platzigram'});
 })
 
@@ -17,8 +18,38 @@ app.get('/signin',function(req, res){
     res.render('index', {title: 'Platzigram - Sign In'});
 })
 
+app.get('/api/pictures', function(req,res){
+	var pictures=[
+		{
+			user:{
+				username:'Anahi',
+				avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
+			},
+			url:'https://image.freepik.com/free-photo/wooden-table-with-blurred-background_1134-14.jpg',
+			likes:0,
+			liked: false,
+			createdAt: +new Date()
+		},
+		{
+			user:{
+				username:'Anahi',
+				avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
+			},
+			url:'https://image.freepik.com/free-photo/working-with-a-coffee_1112-145.jpg',
+			likes:156,
+			liked: true,
+			createdAt: new Date().setDate(new Date().getDate()-10)
+			
+		}
+
+	];
+	setTimeout(function(){
+		res.send(pictures);
+	}, 2000);	
+})
 
 app.listen(3000,function(err){
 	if (err) return console.log('Hubo un error'), process.exit(1);
 	console.log('Platzigram escuchando en el puerto 3000');
 })
+
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,34 +2,23 @@ var page = require('page');
 var empty= require('empty-element');
 var template=require('./template');
 var title=require('title');
+var request= require('superagent');
+var header=require('../header');
 
-page('/', function(ctx,next){
+page('/', header, loadPictures, function(ctx,next){
 	title('Platzigram');
-	var main = document.getElementById('main-container');
-	var pictures=[
-		{
-			user:{
-				username:'Anahi',
-				avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
-			},
-			url:'https://image.freepik.com/free-photo/wooden-table-with-blurred-background_1134-14.jpg',
-			likes:0,
-			liked: false,
-			createdAt: new Date()
+	var main = document.getElementById('main-container');	
 
-		},
-		{
-			user:{
-				username:'Anahi',
-				avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
-			},
-			url:'https://image.freepik.com/free-photo/working-with-a-coffee_1112-145.jpg',
-			likes:156,
-			liked: true,
-			createdAt: new Date().setDate(new Date().getDate()-10)
-			
-		}
-
-	];
-  	empty(main).appendChild(template(pictures));
+  	empty(main).appendChild(template(ctx.pictures));
 });
+
+
+function loadPictures(ctx,next){
+	request
+		.get('/api/pictures')
+		.end(function (err, res){
+			if (err) return console.log(err);
+			ctx.pictures=res.body;
+			next();
+		})
+}
