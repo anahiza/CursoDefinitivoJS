0d515d701ae4a70fdc5c94f3d3962071d85db7c8
Anhy <anahi.z.a@gmail.com>
2016-08-19 10:09:56 -0500

    Agregando un botón para subir una nueva foto

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -3,11 +3,22 @@ var landing = require('../landing');
 var empty= require('empty-element');
 var layout=require('../layout');
 var picture=require('../picture-card');
+var translate=require('../translate');
 
 
 module.exports=function(pictures){
   var el= yo`<div class="container timeline">
   <div class="row">
+    <div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
+      <form enctype="multipart/form-data" class="form-upload" id="form-upload">
+        <div id = "fileName" class="fileUpload btn btn-flat cyan">
+          <span><i class="fa fa-camera" aria-hidden="true"></i>${translate.message('upload-picture')}</span>
+          <input name="picture" id="file" type="file" class="upload" onchange=${onchange}/>
+        </div>    
+        <button id="btnUpload" type = "submit" class="btn btn-flat cyan hide">${translate.message('upload')}</button>
+        <button id="btnCancel" type = "button" class="btn btn-flat red hide" onclick="${cancel}"><i class="fa fa-times" aria-hidden="true"></i></button>
+      </form>
+    </div>
     <div class = "col s12 m10 offset-m1 l6 offset-l3">
       ${pictures.map(function(pic){
         return picture(pic);
@@ -15,5 +26,24 @@ module.exports=function(pictures){
     </div>
   </div>
   </div>`;
+  
+
+  function toggleButtons(){
+    document.getElementById('fileName').classList.toggle('hide');
+    document.getElementById('btnUpload').classList.toggle('hide');
+    document.getElementById('btnCancel').classList.toggle('hide');
+    
+  }
+  function cancel(){
+    toggleButtons();
+    document.getElementById('form-upload').reset();
+
+  }
+
+  function onchange(){
+    toggleButtons();
+  }
+
+
   return layout(el);
 };
