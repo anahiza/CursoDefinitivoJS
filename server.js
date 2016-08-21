c9264dc5a60bec340491a876ee5283d333fa855d
Anahi <anahi.z.a@gmail.com>
2016-08-21 12:08:36 -0500

    Subiendo la foto a nuestro servidor web

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 var express=require ('express');
 var app=express();
+var ext = require('file-extension');
+
+var multer  = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now()+'.' + ext(file.originalname))
+  }
+})
+ 
+var upload = multer({ storage: storage }).single('picture');
+
 
 app.set('view engine','pug');
 
@@ -47,9 +62,17 @@ app.get('/api/pictures', function(req,res){
 		res.send(pictures);
 	}, 2000);	
 })
-
-app.listen(3000,function(err){
-	if (err) return console.log('Hubo un error'), process.exit(1);
-	console.log('Platzigram escuchando en el puerto 3000');
+app.post('/api/pictures', function(req,res){
+    upload(req,res, function(err){
+        if (err){
+            return res.send(500, "Error uploading file");
+        }
+        res.sed("File uploaded");
+        
+    })
 })
 
+app.listen(3000,function(err){
+    if (err) return console.log('Hubo un error'), process.exit(1);
+    console.log('Platzigram escuchando en el puerto 3000');
+})
