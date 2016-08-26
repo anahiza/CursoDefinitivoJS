fa6d82ec8976d48ea0d6a08ba023785fb0847c44
Anhy <anahi.z.a@gmail.com>
2016-08-26 18:27:31 -0500

    Iniciando la pantalla de usuario

--- src/user/index.js
+++ src/user/index.js
@@ -1,7 +1,7 @@
 var empty=require('empty-element');
 var page=require('page');
 var title=require('title');
-var template = require('template');
+var template = require('./template');
 var header = require('../header');
 
 page('/user', header, function(ctx, next){
--- src/user/template.js
+++ src/user/template.js
@@ -4,13 +4,14 @@ var translate=require('../translate');
 
 
 var userProfile = yo`
-	<div class="row">Contenido
+	<div class="row">
 		<div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
 			<div class="row profile">
-				<div class="col s12 m10 l2">
-					<img src="https://pbs.twimg.com/profile_images/308830576/hand_400x400.jpg" class="userPicture"/>
-
-
+				<div class="col s10 offset-s1 m8 offset-m2 l2">
+					<img src="https://pbs.twimg.com/profile_images/308830576/hand_400x400.jpg" class="circle responsive-img"/>
+				</div>
+				<div class="col s10 offset-s1 m8 offset-m1 l10">
+					Este es el contenido
 				</div>
 
 			</div>
