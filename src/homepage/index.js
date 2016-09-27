3f317f99b69dc33390ad1e3cfac5cbb522fac1c7
Anhy <anahi.z.a@gmail.com>
2016-09-27 14:49:58 -0500

    Agregando funcionalidad de WebCAM

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -5,18 +5,80 @@ var title=require('title');
 var request= require('superagent');
 var header=require('../header');
 var axios = require('axios');
+var Webcam=require('webcamjs');
+var picture=require('../picture-card');
 
 page('/', header, loading, asyncLoad, function(ctx,next){
 	title('Platzigram');
 	var main = document.getElementById('main-container');	
 
   	empty(main).appendChild(template(ctx.pictures));
-});
+  const picturePreview = document.getElementById('picture-preview')
+  const camaraInput = document.getElementById('camara-input')
+  const cancelPicture = $('#cancelPicture');
+  const shootButton = $('#shoot');
+  const uploadButton = $('#uploadButton')
+
+  function reset() {
+    picturePreview.classList.add('hide');
+    camaraInput.classList.remove('hide');
+    cancelPicture.addClass('hide');
+    shootButton.removeClass('hide');
+    uploadButton.addClass('hide');
+  }
+
+  cancelPicture.click(reset);
+	
+
+  	$('.modal-trigger').leanModal({
+  		ready: function() {
+  			Webcam.set({
+        		width: 320,
+        		height: 240,
+    		});
+  			
+  			Webcam.attach('#camara-input');
+  			shootButton.click((ev)=>{
+  				Webcam.snap((data_uri)=>{
+                	 picturePreview.innerHTML = `<img src="${data_uri}"/>`;
+          				picturePreview.classList.remove('hide');
+				          camaraInput.classList.add('hide');
+				          cancelPicture.removeClass('hide');
+				          shootButton.addClass('hide');
+				          uploadButton.removeClass('hide')
+				          uploadButton.off('click')
+                	
+                	uploadButton.click(()=>{
+                		const pic ={
+                			url:data_uri,
+                			likes:0,
+                			liked: false,
+                			createdAt: +new Date(),
+                			user:{
+                				username: 'Anahi',
+								avatar: 'https://pbs.twimg.com/profile_images/308830576/hand_400x400.jpg',
+                			}
+
+                		}
+                		$('#picture-cards').prepend(picture(pic));
+                		reset()
+                		$('#modalCamara').closeModal();
+                	});
+            	});
+  			})
+  		},
+  		complete: function(){
+  			Webcam.reset();
+  			reset();
+  		}
+  	})
+})
 
 function loading(ctx,next){
 	var el=document.createElement('div');
 	el.classList.add("loader");
 	var main = document.getElementById('main-container').appendChild(el);
+
 	next();
 }
 
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -9,18 +9,39 @@ var request=require('superagent');
 
 module.exports=function(pictures){
   var el= yo`<div class="container timeline">
+  <div id="modalCamara" class="modal center-align">
+      <div class="modal-content">
+        <div class="camara-picture" id="camara-input"></div>
+        <div class="camara-picture hide" id="picture-preview"></div>
+      </div>
+      <div class="modal-footer">
+        <button class="waves-effect waves-light btn" id="shoot">
+          <i class="fa fa-camera"></i>
+        </button>
+        <button class="waves-effect waves-light btn cyan hide" id="uploadButton">
+          <i class="fa fa-cloud-upload"></i>
+        </button>
+        <button class="waves-effect waves-light btn red hide" id="cancelPicture">
+          <i class="fa fa-times"></i>
+        </button>
+      </div>
+    </div>
+
   <div class="row">
     <div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
       <form enctype="multipart/form-data" class="form-upload" id="form-upload" onsubmit=${onsubmit}>
-        <div id = "fileName" class="fileUpload btn btn-flat cyan">
-          <span><i class="fa fa-camera" aria-hidden="true"></i>${translate.message('upload-picture')}</span>
+        <a href="#modalCamara" class="waves-effect waves-light btn modal-trigger">
+          <i class="fa fa-camera"></i>
+        </a>
+        <div id = "fileName" class="fileUpload btn cyan">
+          <span><i class="fa fa-cloud-upload" aria-hidden="true"></i>${translate.message('upload-picture')}</span>
           <input name="picture" id="file" type="file" class="upload" onchange=${onchange}/>
         </div>    
         <button id="btnUpload" type = "submit" class="btn btn-flat cyan hide">${translate.message('upload')}</button>
         <button id="btnCancel" type = "button" class="btn btn-flat red hide" onclick=${cancel}><i class="fa fa-times" aria-hidden="true"></i></button>
       </form>
     </div>
-    <div class = "col s12 m10 offset-m1 l6 offset-l3">
+    <div class = "col s12 m10 offset-m1 l6 offset-l3" id="picture-cards">
       ${pictures.map(function(pic){
         return picture(pic);
       })}
