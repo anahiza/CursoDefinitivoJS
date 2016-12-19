231192c9cf92cf18be325e74fa3a633a34d38df3
Anahi <anahi.z.a@gmail.com>
2016-12-18 21:57:12 -0600

    agregando sdk amazon

---
+++ config.js
@@ -0,0 +1,10 @@
+'use strict'
+const config={
+
+	aws:{
+		accesKey: process.env.AWS_ACCESS_KEY,
+		secretKey: process.env.AWS_SECRET_KEY
+	}
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -3,15 +3,28 @@ var app=express();
 var ext = require('file-extension');
 
 var multer  = require('multer');
+var aws = require('aws-sdk');
+var multerS3 = require('multer-s3');
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, +Date.now()+'.' + ext(file.originalname))
-  }
+var config = require('./config');
+
+var s3 = new aws.S3({
+	accessKeyId: config.aws.accessKey,
+	secretAccessKey: config.aws.secretKey
 })
+
+var storage = multerS3({
+	s3: s3,
+	bucket: 'anhy-platzigram',
+	acl: 'public-read',
+	metadata: function(req, file, cb){
+		cb(null,{fieldName: file.fieldName})
+	},
+	key: function(req,file,cb){
+		cb(null, +Date.now()+'.' + ext(file.originalname))
+	}
+})
+
  
 var upload = multer({ storage: storage }).single('picture');
 
@@ -65,7 +78,7 @@ app.post('/api/pictures', function(req,res){
         if (err){
             return res.send(500, "Error uploading file");
         }
-        res.sed("File uploaded");
+        res.send("File uploaded");
         
     })
 })
